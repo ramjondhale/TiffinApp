9e8c0628d745567063f485f5afe2bff6a2d9e20f
Ram Jondhale <31801085+ramjondhale@users.noreply.github.com>
2019-08-01 20:45:06 +0530

    Sign Up Complete

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,16 +1,60 @@
 
-import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+ import React, { Component } from 'react';
+ import { Text, View } from 'react-native';
+ //import Icon from 'react-native-vector-icons/MaterialIcons';
+ import TabBar from '@mindinventory/react-native-tab-bar-interaction';
+
+// class Home extends Component {
+//     render() {
+//          return (
+//             <View>
+//             <Text> hello </Text> 
+//             </View>
+//          );
+//     }
+// }
+
+// export default Home;
+
 
 class Home extends Component {
-    render() {
-         return (
-            <View>
-            <Text> hello </Text> 
-            </View>
-         );
+      render() {
+      return (      
+          
+           <TabBar>
+            <TabBar.Item
+                icon={require('./home1.png')}
+                selectedIcon={require('./home.png')}
+                title="Home"
+                screenBackgroundColor={{ backgroundColor: '#008080' }}
+            >
+              <View>
+                 <Text> Home</Text>
+              </View>
+            </TabBar.Item>
+            <TabBar.Item
+                icon={require('./sub1.png')}
+                selectedIcon={require('./sub2.png')}
+                title="Subscriptions"
+                screenBackgroundColor={{ backgroundColor: '#F08080' }}
+            >
+                <View>
+                   <Text>Subscriptions</Text>
+                </View>
+            </TabBar.Item>
+            <TabBar.Item
+                icon={require('./acc1.png')}
+                selectedIcon={require('./acc2.png')}
+                title="Account"
+                screenBackgroundColor={{ backgroundColor: '#485d72' }}
+            >
+              <View>
+                  <Text>Account</Text>
+              </View>
+            </TabBar.Item>
+          </TabBar>
+      );
     }
 }
 
-export default Home;
-
+    export default Home;
